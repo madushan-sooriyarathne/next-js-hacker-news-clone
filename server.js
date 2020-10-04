3cdb350e2d4e04fa9235f52c65b82f54eacb0229
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-10-04 10:55:07 +0530

    updated server.js with fs.craeteReadStream().pipe(res) instead of next's serveStatic() method

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const next = require("next");
 const http = require("http");
 const url = require("url");
 const path = require("path");
+const fs = require("fs");
 
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3000;
@@ -22,8 +23,19 @@ app.prepare().then(() => {
       // if the service-worker.js is requested, serve it as a static file
       if (pathname === "/service-worker.js") {
         const filepath = path.join(__dirname, ".next", pathname);
-        console.log(filepath);
-        app.serveStatic(req, res, filepath);
+
+        // check if the file exists in the system
+        try {
+          if (fs.existsSync(filepath)) {
+            console.log(`${pathname} exists in the system. Sending the file`);
+            res.setHeader("content-type", "text/javascript");
+            fs.createReadStream(filepath).pipe(res);
+            // app.serveStatic(req, res, filepath);
+          }
+        } catch (error) {
+          console.warn(`${pathname} does not exists in ${filepath}`);
+          res.status(404).send("File not found");
+        }
 
         // else let next handle the requests
       } else {
